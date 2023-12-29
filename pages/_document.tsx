9a0a3b637453dabd3e0bb9f5492f9ffca36c41d3
easeplan <easeplan.team@gmail.com>
2023-12-29 10:37:14 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <link rel="shortcut icon" href="/easeplanlogo.png"></link>
+          <link rel="apple-touch-icon" href="/easeplanlogo.png"></link>
+          <link
+            rel="apple-touch-icon-precomposed"
+            href="/easeplanlogo.png"
+          ></link>
+          <meta name="next-head-count" content="24"></meta>
+          <meta name="theme-color" content="#134153" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
         <meta
           name="google-site-verification"
           content="lUaVCkoNpMHRTdWtbNnBrMl0585RRJlDTTKFhZ0Itfw"
