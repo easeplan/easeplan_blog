d04d77df41bccd5bd68dcd2f6266959e3942e132
easeplan <easeplan.team@gmail.com>
2023-12-29 10:56:24 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,6 +48,47 @@ export default function Home({ posts, currentPage, numPages }: any) {
           Latest Industry News, Interviews, and Resources for Your Event
           Business | Easeplan Blog
         </title>
+        <meta
+          name="description"
+          content="Explore the latest industry news, insightful interviews, and valuable resources to help your event business thrive. Visit Easeplan Blog today for expert insights and updates."
+        />
+  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content="Latest Industry News, Interviews, and Resources for Your Event Business | Easeplan Blog"
+        />
+        <meta
+          property="og:description"
+          content="Explore the latest industry news, insightful interviews, and valuable resources to help your event business thrive. Visit Easeplan Blog today for expert insights and updates."
+        />
+        <meta
+          property="og:url"
+          content="https://blog.easeplan.io"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dw8my5zef/image/upload/v1703842341/r0gqekuqsfjfbkqyv1o8.png"
+        />
+        <meta property="og:type" content="article" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Latest Industry News, Interviews, and Resources for Your Event Business | Easeplan Blog"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore the latest industry news, insightful interviews, and valuable resources to help your event business thrive. Visit Easeplan Blog today for expert insights and updates."
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dw8my5zef/image/upload/v1703842341/r0gqekuqsfjfbkqyv1o8.png"
+        />
+        <meta
+          name="keywords"
+          content="event industry news, event business resources, event business interviews, Easeplan Blog"
+        />
       </Head>
       <main>
         <HeroSection handleSearch={handleSearch} searchQuery={searchQuery} />
