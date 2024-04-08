d38cb579c04d98b0377beb9c116e98be18402c56
icode247 <zionekekenta@gmail.com>
2024-04-08 10:28:55 +0100

    add SEO tool

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,11 @@ export default function Document() {
             />
           </>
         )}
+        <script
+          src="https://assets.usestyle.ai/seonajsplugin"
+          defer
+          id="seona-js-plugin"
+        ></script>
       </Head>
       <body>
         <Main />
